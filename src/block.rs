81486ff0f9d3b6a61124075cf65edb5e621af5e4
Dmitry Polyanitsa <coffeecup.winner@gmail.com>
2022-09-02 15:15:40 +0200

    Moves implementation

--- src/block.rs
+++ src/block.rs
@@ -1,10 +1,18 @@
-pub struct Rectangle {
-    x: u32,
-    y: u32,
-    w: u32,
-    h: u32,
+#[derive(Clone, PartialEq, Eq)]
+pub struct Rect {
+    pub x: u32,
+    pub y: u32,
+    pub w: u32,
+    pub h: u32,
 }
 
+impl Rect {
+    pub fn new(x: u32, y: u32, w: u32, h: u32) -> Self {
+        Rect { x, y, w, h }
+    }
+}
+
+#[derive(Clone, Copy, PartialEq, Eq)]
 pub struct Color {
     r: u8,
     g: u8,
@@ -12,9 +20,25 @@ pub struct Color {
     a: u8,
 }
 
+pub type BlockId = Vec<u32>;
+
 pub enum Block {
-    SimpleBlock(Rectangle, Color),
-    ComplexBlock(Rectangle, Vec<Block>),
+    SimpleBlock(BlockId, Rect, Color),
+    ComplexBlock(BlockId, Rect, Vec<Block>),
 }
 
-pub type BlockId = Vec<u32>;
+impl Block {
+    pub fn id(&self) -> &BlockId {
+        match self {
+            Block::SimpleBlock(id, _, _) => id,
+            Block::ComplexBlock(id, _, _) => id,
+        }
+    }
+
+    pub fn rect(&self) -> &Rect {
+        match self {
+            Block::SimpleBlock(_, rect, _) => rect,
+            Block::ComplexBlock(_, rect, _) => rect,
+        }
+    }
+}
--- src/moves.rs
+++ src/moves.rs
@@ -1,4 +1,4 @@
-use crate::block::{BlockId, Color};
+use crate::block::{BlockId, Color, Block, Rect};
 
 pub enum Orientation {
     Horizontal,
@@ -22,5 +22,105 @@ impl Move {
             Move::Swap(_, _) => 3,
             Move::Merge(_, _) => 1,
         }
+    }
+
+    pub fn line_cut(block: &mut Block, orientation: Orientation, offset: u32) -> Self {
+        let id = block.id().clone();
+        match block {
+            Block::SimpleBlock(id, rect, color) => {
+                let (b0, b1) = match orientation {
+                    Orientation::Horizontal => {
+                        let r0 = Rect::new(rect.x, rect.y, rect.w, offset);
+                        let r1 = Rect::new(rect.x, rect.y, rect.w, rect.h - offset);
+                        let mut id0 = id.clone();
+                        id0.push(0);
+                        let mut id1 = id.clone();
+                        id1.push(1);
+                        (
+                            Block::SimpleBlock(id0, r0, *color),
+                            Block::SimpleBlock(id1, r1, *color),
+                        )
+                    }
+                    Orientation::Vertical => {
+                        let r0 = Rect::new(rect.x, rect.y, offset, rect.h);
+                        let r1 = Rect::new(rect.x, rect.y, rect.w - offset, rect.h);
+                        let mut id0 = id.clone();
+                        id0.push(0);
+                        let mut id1 = id.clone();
+                        id1.push(1);
+                        (
+                            Block::SimpleBlock(id0, r0, *color),
+                            Block::SimpleBlock(id1, r1, *color),
+                        )
+                    }
+                };
+                *block = Block::ComplexBlock(id.clone(), rect.clone(), vec![b0, b1]);
+            }
+            Block::ComplexBlock(_, _, _) => panic!("Invalid block"),
+        }
+        Move::LineCut(id, orientation, offset)
+    }
+
+    pub fn point_cut(block: &mut Block, offset_x: u32, offset_y: u32) -> Self {
+        let id = block.id().clone();
+        match block {
+            Block::SimpleBlock(id, rect, color) => {
+                let r0 = Rect::new(rect.x, rect.y, offset_x, offset_y);
+                let r1 = Rect::new(offset_x, rect.y, rect.w - offset_x, offset_y);
+                let r2 = Rect::new(offset_x, offset_y, rect.w - offset_x, rect.h - offset_y);
+                let r3 = Rect::new(rect.x, offset_y, offset_x, rect.h - offset_y);
+                let mut id0 = id.clone();
+                id0.push(0);
+                let mut id1 = id.clone();
+                id1.push(1);
+                let mut id2 = id.clone();
+                id1.push(2);
+                let mut id3 = id.clone();
+                id1.push(3);
+                let b0 = Block::SimpleBlock(id0, r0, *color);
+                let b1 = Block::SimpleBlock(id1, r1, *color);
+                let b2 = Block::SimpleBlock(id2, r2, *color);
+                let b3 = Block::SimpleBlock(id3, r3, *color);
+                *block = Block::ComplexBlock(id.clone(), rect.clone(), vec![b0, b1, b2, b3]);
+            }
+            Block::ComplexBlock(_, _, _) => panic!("Invalid block"),
+        }
+        Move::PointCut(id, offset_x, offset_y)
+    }
+
+    pub fn color(block: &mut Block, new_color: Color) -> Self {
+        let id = block.id().clone();
+        match block {
+            Block::SimpleBlock(_, _, color) => {
+                *color = new_color;
+            }
+            Block::ComplexBlock(_, _, _) => panic!("Invalid block"),
+        }
+        Move::Color(id, new_color)
+    }
+
+    pub fn swap(block0: &mut Block, block1: &mut Block) -> Self
+    {
+        assert!(block0.rect() == block1.rect());
+        std::mem::swap(block0, block1);
+        Move::Swap(block1.id().clone(), block0.id().clone())
+    }
+
+    pub fn merge(block0: Block, block1: Block) -> (Block, Self)
+    {
+        let r0 = block0.rect();
+        let r1 = block1.rect();
+        let vertically_adjacent = r0.w == r1.w && ((r0.y + r0.h == r1.y) || (r0.y == r1.y + r1.h));
+        let horizontally_adjacent = r0.h == r1.h && ((r0.x + r0.w == r1.x) || (r0.x == r1.x + r1.w));
+        assert!(vertically_adjacent || horizontally_adjacent);
+        let id0 = block0.id().clone();
+        let id1 = block1.id().clone();
+        let rect = if vertically_adjacent {
+            Rect::new(r0.x, r0.y.min(r1.y), r0.w, r0.h + r1.h)
+        } else {
+            Rect::new(r0.x.min(r1.x), r1.y, r0.w + r1.w, r0.h)
+        };
+        let new_block = Block::ComplexBlock(BlockId::new(), rect, vec![block0, block1]);
+        (new_block, Move::Merge(id0, id1))
     }
 }
