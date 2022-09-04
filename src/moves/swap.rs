832484b970c1a189cbb7057ca825a648abb86aa4
Dmitry Polyanitsa <coffeecup.winner@gmail.com>
2022-09-04 05:02:32 +0200

    Core: Fix swap for complex blocks

--- src/moves/swap.rs
+++ src/moves/swap.rs
@@ -1,4 +1,4 @@
-use crate::block::BlockId;
+use crate::block::{BlockData, BlockId};
 use crate::canvas::Canvas;
 use crate::moves::{Cost, Move, MoveError, UndoMove};
 
@@ -25,7 +25,34 @@ pub fn swap(
         )));
     }
 
+    let x_diff = block_a
+        .r
+        .bottom_left
+        .x
+        .wrapping_sub(block_b.r.bottom_left.x);
+    let y_diff = block_a
+        .r
+        .bottom_left
+        .y
+        .wrapping_sub(block_b.r.bottom_left.y);
+
     std::mem::swap(&mut block_a.data, &mut block_b.data);
+    if let BlockData::Complex(bs) = &mut block_a.data {
+        for b in bs {
+            b.r.bottom_left.x = b.r.bottom_left.x.wrapping_add(x_diff);
+            b.r.bottom_left.y = b.r.bottom_left.y.wrapping_add(y_diff);
+            b.r.top_right.x = b.r.top_right.x.wrapping_add(x_diff);
+            b.r.top_right.y = b.r.top_right.y.wrapping_add(y_diff);
+        }
+    }
+    if let BlockData::Complex(bs) = &mut block_b.data {
+        for b in bs {
+            b.r.bottom_left.x = b.r.bottom_left.x.wrapping_sub(x_diff);
+            b.r.bottom_left.y = b.r.bottom_left.y.wrapping_sub(y_diff);
+            b.r.top_right.x = b.r.top_right.x.wrapping_sub(x_diff);
+            b.r.top_right.y = b.r.top_right.y.wrapping_sub(y_diff);
+        }
+    }
 
     canvas.put_block(block_a);
     canvas.put_block(block_b);
