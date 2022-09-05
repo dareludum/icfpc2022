28f672096117128d5f691b52fb0959122b41ccfb
Victor "multun" Collod <victor.collod@epita.fr>
2022-09-05 12:51:23 +0200

    remove ded c0d

--- src/cmd/default.rs
+++ src/cmd/default.rs
@@ -1,10 +1,9 @@
 use std::{
     fs,
-    path::{Path, PathBuf},
+    path::PathBuf,
 };
 
 use crate::{
-    dto::SolvedSolutionDto,
     gui::gui_main,
     moves::Move,
     solvers::Problem,
@@ -95,101 +94,6 @@ fn solve(
         .par_iter()
         .map(|problem_path| solve_problem(&solvers, &base_solution_dir, problem_path))
         .collect::<std::io::Result<()>>()
-}
-
-fn win_indicator_str(old: u64, new: u64) -> String {
-    if new < old {
-        "!!! WE ARE WINNING SON !!!".to_string()
-    } else {
-        "".to_string()
-    }
-}
-
-fn write_best(
-    base_dir: &Path,
-    problem_num: &str,
-    solution: &SolvedSolutionDto,
-) -> std::io::Result<()> {
-    let target_dir = base_dir.join("best");
-    let current_solution_dir = base_dir.join("current").join(solution.solver_name.clone());
-
-    std::fs::create_dir_all(&target_dir)?;
-    let best = get_best_solution(solution, base_dir, problem_num)?;
-    match best {
-        (_, false) => Ok(()),
-        _ => copy_output(current_solution_dir, target_dir, problem_num),
-    }
-}
-
-fn fcopy_to(current_dir: &Path, target_dir: &Path, filename: &str) -> std::io::Result<()> {
-    let fname_from = current_dir.join(filename);
-    let fname_to = target_dir.join(filename);
-
-    std::fs::copy(fname_from, fname_to)?;
-
-    Ok(())
-}
-
-fn copy_output(
-    current_solution_dir: PathBuf,
-    target_dir: PathBuf,
-    problem_num: &str,
-) -> Result<(), std::io::Error> {
-    fcopy_to(
-        &current_solution_dir,
-        &target_dir,
-        &format!("{problem_num}_meta.json"),
-    )?;
-
-    fcopy_to(
-        &current_solution_dir,
-        &target_dir,
-        &format!("{problem_num}.png"),
-    )?;
-
-    fcopy_to(
-        &current_solution_dir,
-        &target_dir,
-        &format!("{problem_num}.txt"),
-    )?;
-
-    Ok(())
-}
-
-fn get_best_solution(
-    current_solution: &SolvedSolutionDto,
-    base_dir: &Path,
-    problem_num: &str,
-) -> std::io::Result<(SolvedSolutionDto, bool)> {
-    read_current_best(base_dir, problem_num).map(|best| match best {
-        Some(current_best) => {
-            if current_solution.total_score < current_best.total_score {
-                (current_solution.clone(), true)
-            } else {
-                (current_best, false)
-            }
-        }
-        None => (current_solution.clone(), true),
-    })
-}
-
-fn read_current_best(
-    base_dir: &Path,
-    problem_num: &str,
-) -> std::io::Result<Option<SolvedSolutionDto>> {
-    let meta_fname = &format!("{problem_num}_meta.json");
-    let best_solution_filename = base_dir.join("best").join(meta_fname);
-
-    match best_solution_filename.try_exists() {
-        Ok(false) => Ok(None),
-        Ok(true) => {
-            let current_best_json = fs::read_to_string(best_solution_filename)?;
-            let current_best: SolvedSolutionDto =
-                serde_json::from_str(&current_best_json).expect("Deserialization error");
-            Ok(Some(current_best))
-        }
-        Err(e) => Err(e),
-    }
 }
 
 pub fn default_command(
